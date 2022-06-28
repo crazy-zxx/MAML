d68405325b71c00f72932232c3b369cb570def57
zhao <1934109821@qq.com>
2022-06-28 13:12:24 +0800

    Initial commit

---
+++ init.sh
@@ -0,0 +1,3 @@
+export nnUNet_raw_data_base="/datasets/data/nnUNet_raw_data_base"
+export nnUNet_preprocessed="/datasets/data/nnUNet_preprocessed"
+export RESULTS_FOLDER="/datasets/data/nnUNet_trained_models"
